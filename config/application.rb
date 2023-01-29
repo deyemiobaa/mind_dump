9d069ab73830d3f822d5b8f8482f192643158b69
sodiq <sodiqaderibigbe90@gmail.com>
2023-01-29 23:14:33 +0100

    set uuid as default id gen

--- config/application.rb
+++ config/application.rb
@@ -20,5 +20,9 @@ module MindDump
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    # setup UUIDs for all models
+    config.generators do |g|
+      g.orm :active_record, primary_key_type: :uuid
+    end
   end
 end
