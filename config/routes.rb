d221c12b486606718f4dcd870b0d1fcfd931f08b
sodiq <sodiqaderibigbe90@gmail.com>
2023-02-04 20:00:43 +0100

    configure new routes for user registrations

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  devise_for :users, controllers: { registrations: "registrations" }
+
   root 'welcome#index'
-  devise_for :users
+  get 'welcome', to: 'call_to_action#after_signup'
 end
