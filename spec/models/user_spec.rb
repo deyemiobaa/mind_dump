4e040dd348d25444b2f35458ec2be895a0601dcb
sodiq <sodiqaderibigbe90@gmail.com>
2023-02-05 16:33:45 +0100

    add extra example to model spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,8 +5,18 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
   describe 'default_username' do
     it 'returns the username' do
-      user = FactoryBot.build(:user)
+      user = FactoryBot.create(:user, :regular)
       expect(user.default_username).to eq(user.email.split('@').first)
+    end
+
+    it 'setting the username works' do
+      user = FactoryBot.build(:user, :regular)
+
+      expect(user.username).to eq ""
+
+      user.set_default_username
+
+      expect(user.username).to eq(user.email.split('@').first)
     end
   end
 end
