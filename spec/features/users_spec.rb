61fff6df95cfe8d84cb8c7afa4e86fe872ab5356
sodiq <sodiqaderibigbe90@gmail.com>
2023-02-05 16:33:27 +0100

    add spec to login user

--- spec/features/users_spec.rb
+++ spec/features/users_spec.rb
@@ -4,7 +4,7 @@ require 'rails_helper'
 
 RSpec.describe 'Users', type: :feature do
   describe 'Get /users/sign_up' do
-    it 'returns valid response' do
+    it 'user can sign up' do
       visit new_user_registration_path
 
       fill_in 'Email', with: 'email@example.com'
@@ -13,6 +13,21 @@ RSpec.describe 'Users', type: :feature do
       click_button 'Sign up'
 
       expect(page).to have_content('Thank you for signing up!')
+    end
+  end
+
+  describe 'Get /users/sign_in' do
+    it 'user can log in' do
+      visit new_user_session_path
+
+      user = FactoryBot.create(:user, :confirmed)
+
+      fill_in 'Email', with: user.email
+      fill_in 'Password', with: 'password'
+
+      click_button 'Log in'
+
+      expect(page).to have_content('Signed in successfully.')
     end
   end
 end
